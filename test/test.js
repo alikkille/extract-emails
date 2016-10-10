1f69d38aef2a7972070c5e5ca4bf7e1286d507d2
Vlad Dm <2tunnels@gmail.com>
2016-10-11 00:20:07 +0200

    tweak

--- test/test.js
+++ test/test.js
@@ -1,46 +1,48 @@
-'use strict';
+(() => {
+    'use strict';
 
-const assert = require('assert');
-const extractEmails = require('./../src/extract-emails');
+    const assert = require('assert');
+    const extractEmails = require('./../src/extract-emails');
 
-describe('#extractEmails', () => {
-    it('should throw an error if argument not provided', () => {
-        assert.throws(
-            () => {
-                extractEmails();
-            },
-            Error
-        );
+    describe('#extractEmails', () => {
+        it('should throw an error if argument not provided', () => {
+            assert.throws(
+                () => {
+                    extractEmails();
+                },
+                Error
+            );
+        });
+
+        it('should throw an error if argument is not a string', () => {
+            assert.throws(
+                () => {
+                    extractEmails(1);
+                },
+                Error
+            );
+        });
+
+        it('should return empty array if emails not found', () => {
+            let actual = extractEmails('<p>Text without emails</p>');
+            assert.deepEqual([], actual);
+        });
+
+        it('should return array of emails from html', () => {
+            let expected, actual;
+
+            // single email
+            expected = ['john@example.com'];
+            actual = extractEmails('<p>john@example.com</p>');
+            assert.deepEqual(expected, actual);
+
+            // multiple emails
+            expected = ['bill@example.net', 'jack@example.edu'];
+            actual = extractEmails(`
+              <p>Lorem ipsum dolor bill@example.net nostrud.</p>
+              <p>Lorem ipsum dolor sit amet, jack@example.edu adipisicing elit.</p>
+            `);
+            assert.deepEqual(expected, actual);
+        });
     });
-
-    it('should throw an error if argument is not a string', () => {
-      assert.throws(
-          () => {
-              extractEmails(1);
-          },
-          Error
-      );
-    });
-
-    it('should return empty array if emails not found', () => {
-        let actual = extractEmails('<p>Text without emails</p>');
-        assert.deepEqual([], actual);
-    });
-
-    it('should return array of emails from html', () => {
-        let expected, actual;
-
-        // single email
-        expected = ['john@example.com'];
-        actual = extractEmails('<p>john@example.com</p>');
-        assert.deepEqual(expected, actual);
-
-        // multiple emails
-        expected = ['bill@example.net', 'jack@example.edu'];
-        actual = extractEmails(`
-          <p>Lorem ipsum dolor bill@example.net nostrud.</p>
-          <p>Lorem ipsum dolor sit amet, jack@example.edu adipisicing elit.</p>
-        `);
-        assert.deepEqual(expected, actual);
-    });
-});
+})();
