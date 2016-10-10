8d1935ff101dfa1dfff5b7d01e15f074ddab9eea
Vlad Dm <2tunnels@gmail.com>
2016-10-10 21:26:34 +0200

    extract plain emails

--- extract-emails.js
+++ extract-emails.js
@@ -1,8 +1,22 @@
 (function() {
     'use strict';
 
+    const emailRegExp = /(\w+@\w+\.\w+)/ig;
+
+    function extractPlainEmails(html) {
+        let emails = [];
+
+        let match;
+
+        while ((match = emailRegExp.exec(html)) !== null) {
+            emails.push(match[1]);
+        }
+
+        return emails;
+    }
+
     function extractEmails(html) {
-        return ['user@example.com'];
+        return extractPlainEmails(html);
     }
 
     module.exports = extractEmails;
--- test/test.js
+++ test/test.js
@@ -2,10 +2,20 @@ const assert = require('assert');
 const extractEmails = require('./../extract-emails');
 
 describe('#extractEmails', () => {
-    it('should return emails from plain text', () => {
-        expected = ['user@example.com'];
-        actual = extractEmails('<p>user@example.com</p>');
+    it('should return emails from html', () => {
+        let expected, actual;
 
+        // single email
+        expected = ['john@example.com'];
+        actual = extractEmails('<p>john@example.com</p>');
+        assert.deepEqual(expected, actual);
+
+        // multiple emails
+        expected = ['bill@example.net', 'jack@example.edu'];
+        actual = extractEmails(`
+          <p>Lorem ipsum dolor bill@example.net nostrud.</p>
+          <p>Lorem ipsum dolor sit amet, jack@example.edu adipisicing elit.</p>
+        `);
         assert.deepEqual(expected, actual);
     });
 });
